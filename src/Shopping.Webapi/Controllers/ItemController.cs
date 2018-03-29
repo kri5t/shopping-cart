be1759c262f172bdbd0a8e395f9303c039e7f191
Kristian Barrett <kba@firstagenda.com>
2018-03-29 16:37:14 +0200

    * Added item controller

---
+++ src/Shopping.Webapi/Controllers/ItemController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping.Webapi.Controllers
+{
+    [Route("api/v1/shoppingcart/{shoppingcartuid}/item")]
+    public class ItemController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public ItemController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get(bool includeItems = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{uid}")]
+        public async Task<IActionResult> Get(Guid uid)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{uid}")]
+        public void Delete(Guid uid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
