f9dde1d8e0e41f99c2d0bda5fa42220d31d06577
Kristian Barrett <kba@firstagenda.com>
2018-03-29 19:26:52 +0200

    * Added delete command * Added delete command unit tests

---
+++ src/Shopping.Core/Commands/Item/DeleteItemCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Core.Infrastructure.Mediation;
+using Shopping.Database;
+
+namespace Shopping.Core.Commands.Item
+{
+    public class DeleteItemCommand : IRequest<VoidResponse>
+    {
+        public Guid Uid { get; }
+
+        public DeleteItemCommand(Guid uid)
+        {
+            Uid = uid;
+        }
+    }
+    
+    [UsedImplicitly]
+    public class DeleteItemCommandHandler : BaseHandler<DeleteItemCommand, VoidResponse>
+    {
+        private readonly DatabaseContext _databaseContext;
+        
+        public DeleteItemCommandHandler(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+        
+        public override async Task<VoidResponse> Handle(DeleteItemCommand request, CancellationToken cancellationToken)
+        {
+            var item =  await _databaseContext.Items.SingleOrDefaultAsync(i => i.Uid == request.Uid, cancellationToken);
+            if(item == null)
+                return Error(ErrorCode.NotFound, $"Not able to find item with {request.Uid}");
+            _databaseContext.Remove(item);
+            await _databaseContext.SaveChangesAsync(cancellationToken);
+            return new VoidResponse();
+        }
+    }
+}
---
+++ src/Shopping.UnitTest/Commands/Item/DeleteItemCommandTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Shopping.Core.Commands.Item;
+using Shopping.Core.Infrastructure.Mediation;
+using Shopping.Database;
+using Shopping.UnitTest.Helpers;
+using Shopping.UnitTest.Infrastructure;
+using Xunit;
+
+namespace Shopping.UnitTest.Commands.Item
+{
+    public class DeleteItemCommandTest : EfContextTest<DatabaseContextFactory, DatabaseContext>
+    {
+        private readonly DatabaseContext _dbContext;
+        private readonly DeleteItemCommandHandler _sut;
+        private readonly Guid _uid = Guid.NewGuid();
+        private readonly DateTimeOffset _createdDate = DateTimeOffset.UtcNow;
+        private const string Description = "Something";
+        private const int Quantity = 2;
+
+        public DeleteItemCommandTest()
+        {
+            _dbContext = Context();
+            _dbContext.AddShoppingCartToContext(_createdDate, _uid, true, Description, Quantity);
+            _dbContext.SaveChanges();
+            _sut = new DeleteItemCommandHandler(_dbContext);
+        }
+
+        [Fact]
+        public async Task Delete_item_command_removes_item_from_db()
+        {
+            var result = await _sut.Handle(new DeleteItemCommand(_uid), CancellationToken.None);
+            Assert.False(result.HasError);
+            Assert.Empty(_dbContext.Items);
+        } 
+        
+        [Fact]
+        public async Task Delete_item_command_with_non_existant_item_returns_error()
+        {
+            var result = await _sut.Handle(new DeleteItemCommand(Guid.NewGuid()), CancellationToken.None);
+            
+            Assert.True(result.HasError);
+            Assert.Equal(ErrorCode.NotFound, result.ErrorCode);
+        }
+    }
+}
